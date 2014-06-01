31d6a8e6495dbdff285b4d681ebeba70b4951692
Timo Larjo <timo.larjo@gmail.com>
2014-06-01 21:27:01 +0300

    More testing

--- src/beatdivider/Test.java
+++ src/beatdivider/Test.java
@@ -3,8 +3,9 @@ package beatdivider;
 public class Test {
 	public String blaa() {
 		while(true) {
-			String x = "EhHeHe";
+			String x = "EhHeXXHeYY";
 			System.out.println("Ei näin");
+			System.out.println("No eipä niin");
 			return x;
 		}
 	}
